06ea1e79c364397e2c325caee427fb85b2c3adb9
Cristiano Clemente <32751490+cristiano-clemente@users.noreply.github.com>
2021-01-31 13:56:38 +0000

    changed api url

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-recognition-fullstack.herokuapp.com/register', {
+    fetch('https://rocky-caverns-46196.herokuapp.com/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://face-recognition-fullstack.herokuapp.com/signin', {
+    fetch('https://rocky-caverns-46196.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
